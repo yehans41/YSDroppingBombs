4f292824c00986b8844981fe31dde4fe9aeb4188
BillySwCodeNinjas <116742021+yehans41@users.noreply.github.com>
2023-03-30 19:37:33 -0500

    D
    D

---
+++ YSTrickHead/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Platform Object")]
+    public GameObject platform;
+    [Header("Game Over UI Canvas Object")]
+    public GameObject gameOverCanvas;
+    float pos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            SpawnPlatforms();
+        }
+    }
+
+    void SpawnPlatforms()
+    {
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
+    }
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+    }
+}
