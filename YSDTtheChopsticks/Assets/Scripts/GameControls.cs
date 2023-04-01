c67c47219643da8cb6f4a79c92ed1e34704b4a6b
Andrewkk0139 <116742021+yehans41@users.noreply.github.com>
2023-04-01 14:02:19 -0500

    chop
    chop

---
+++ YSDTtheChopsticks/Assets/Scripts/GameControls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControls: MonoBehaviour
+{
+    private Text timerText;
+    private int timerCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        StartCoroutine(CountTime());
+        timerText = GameObject.Find("Score").GetComponent<Text>();
+    }
+
+    IEnumerator CountTime()
+    {
+        yield return new WaitForSeconds(1f);
+        timerCount++;
+        timerText.text = "Score: " + timerCount;
+        StartCoroutine(CountTime());
+    }
+}
