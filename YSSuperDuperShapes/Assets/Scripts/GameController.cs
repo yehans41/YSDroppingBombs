09c475d237248e08a22b4b997d36ccd93ac2b255
BillySwCodeNinjas <116742021+yehans41@users.noreply.github.com>
2023-04-04 19:46:20 -0500

    shapes
    ss

---
+++ YSSuperDuperShapes/Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Shape Objects")]
+    public GameObject[] shapePrefabs;
+    //Spawn rate for how many
+    //objects are spawned
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 2f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 3f;
+    [Header("Game Over UI Object")]
+    public GameObject gameOverCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Spawn()
+    {
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        //Spawn new hexagon at position
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+    public void GameOver()
+    {
+        CancelInvoke("Spawn");
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
---
+++ YSSuperShapes/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Default Movement Speed ")]
+    public float moveSpeed = 600f;
+    //Movement float
+    float movement = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        //Object transformation rotates around
+        //an object using the settings
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+    }
+}
